021dcda181ad36f5e42984b691bb9960f333396c
aayushb03 <abbharadwaj@wisc.edu>
2024-01-11 13:13:19 -0600

    Bug Fixes and Comments

--- movieAPI.py
+++ movieAPI.py
@@ -3,7 +3,16 @@ import requests
 
 
 class MovieAPI:
-    def __init__(self, api_key):
+    def __init__(self, api_key, num_results):
+        """
+        Initialize MovieAPI instance.
+
+        Parameters:
+        - api_key (str): The API key for accessing the MovieDB API.
+        - num_results (int): Desired number of results.
+        """
+
+        # Set up API url and header
         self.url = (
             "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1"
             "&sort_by=popularity.desc&primary_release_year=")
@@ -13,20 +22,51 @@ class MovieAPI:
             "Authorization": "Bearer " + api_key
         }
 
-    def get_movies(self, year, title_mode, sort_by, num):
-        response = requests.get(self.url + str(year), headers=self.headers)
-        if response.status_code != 200:
+        # Set desired number of results
+        self.num_results = num_results
+
+        # Year and response placeholders
+        self.year = None
+        self.response = None
+
+    def get_movies(self, year, title_mode, sort_by):
+        """
+        Get a DataFrame of movies based on the specified parameters.
+
+        Parameters:
+        - year (int): The release year of the movies.
+        - title_mode (str): Title mode ('Simplified' or 'Full').
+        - sort_by (str): Sort by 'Votes' or 'Title'.
+
+        Returns:
+        - pd.DataFrame or None: DataFrame of movies or None if an error occurs.
+        """
+
+        # Check previous saved year to minimize API calls if year is unchanged
+        if year != self.year:
+            # Get response using year parameter, if year is unchanged, then use previously saved response
+            self.response = requests.get(self.url + str(year), headers=self.headers)
+
+        # Reset year
+        self.year = year
+
+        # Check status code
+        if self.response.status_code != 200:
             return None
 
-        data = response.json()["results"]
+        data = self.response.json()["results"]
+
+        # Validate length of data
         if len(data) == 0:
             return None
 
         movies_data = []
-        for i in range(min(len(data), num)):
+        # Iterate number of times specified, or through all data if length is less than number specified
+        for i in range(min(len(data), self.num_results)):
             movie_dict = {}
             title = data[i]["title"]
-            if title_mode == 'Simplified':
+            if title_mode == "Simplified":
+                # Move "The" and "A" to end of title if "Simplified" mode
                 if title.startswith("The "):
                     title = title[4:] + ", The"
                 elif title.startswith("A "):
@@ -38,9 +78,11 @@ class MovieAPI:
 
             movies_data.append(movie_dict)
 
+        # Convert array of dictionaries to dataframe
         movies_df = pd.DataFrame(movies_data)
 
-        movies_df = movies_df.sort_values(by=sort_by, ascending=False)
+        # Sort data based on "Votes" or "Title", and only sort ascending if by Title
+        movies_df = movies_df.sort_values(by=sort_by, ascending=(sort_by == "Title"))
         movies_df = movies_df.reset_index(drop=True)
 
         return movies_df
--- movieRatingsApp.py
+++ movieRatingsApp.py
@@ -1,21 +1,23 @@
 from movieAPI import MovieAPI
 import streamlit as st
 
+# Initialize constants
 API_KEY = ("eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjhjOTJjNjgyNzlmMjk1NGJmMmZlYzBkNDBmZWU3MCIsInN1YiI6IjY1OWRmNDgxNmQ5N2U2MD"
            "BmMDc4ZTQ2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.J02giUhuBK_NjdwNyS2iOj_7BNhFsWpL2dd4YZ44ExY")
-md = MovieAPI(API_KEY)
-
 NUM_RESULTS = 10
 
-st.write("""
-# Movie Ratings
-Retrieve the top 10 movies from any given year:
-""")
+# Initialize MovieAPI object with specified constants
+md = MovieAPI(API_KEY, NUM_RESULTS)
 
+# Header
+st.header("Movie Ratings App")
+st.write("Retrieve the top 10 movies from any given year:")
+
+# Buttons/Selectors
 wid1, wid2, wid3 = st.columns([1, 1, 1])
 with (wid1):
     year = st.number_input(
-        label='Year:',
+        label="Year:",
         min_value=1000,
         max_value=2024,
         value=2024,
@@ -23,33 +25,30 @@ with (wid1):
     )
 with (wid2):
     title_option = st.radio(
-        label='Title:',
-        options=['Full', 'Simplified'],
+        label="Title:",
+        options=["Full", "Simplified"],
         horizontal=True
     )
 with (wid3):
     sort_option = st.radio(
-        label='Sort By:',
-        options=['Votes', 'Title'],
+        label="Sort By:",
+        options=["Votes", "Title"],
         horizontal=True
     )
 
-movies_data = md.get_movies(year, title_option, sort_option, NUM_RESULTS)
+# Retrieve movie data
+movies_data = md.get_movies(year, title_option, sort_option)
 
+# Show results or error message
 if movies_data is not None:
-    st.write("""
-    *Table Preview:*
-    """)
+    st.write("*Table Preview:*")
     st.table(movies_data)
 
     st.download_button(
         label="Download CSV",
-        data=movies_data.to_csv(index=False).encode('utf-8'),
+        data=movies_data.to_csv(index=False).encode("utf-8"),
         file_name="movie_ratings_" + str(year) + ".csv",
-        mime='text/csv'
+        mime="text/csv"
     )
 else:
-    st.write(
-        """
-        No Results Exist!
-        """)
+    st.write("No Results Exist!")
